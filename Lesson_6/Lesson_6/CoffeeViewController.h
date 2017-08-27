21aa7614014f2413f78042a46bfb81ec8212a449
Andrey Proskurin <andrewproskurin@gmail.com>
2017-08-27 16:33:28 +0300

    Добавлен экран для домашнего задания

---
+++ Lesson_6/Lesson_6/CoffeeViewController.h
@@ -0,0 +1,13 @@
+//
+//  CoffeeViewController.h
+//  Lesson_6
+//
+//  Created by Andrey Proskurin on 27.08.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoffeeViewController : UIViewController
+
+@end
---
+++ Lesson_6/Lesson_6/CoffeeViewController.m
@@ -0,0 +1,27 @@
+//
+//  CoffeeViewController.m
+//  Lesson_6
+//
+//  Created by Andrey Proskurin on 27.08.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import "CoffeeViewController.h"
+
+@interface CoffeeViewController ()
+
+@end
+
+@implementation CoffeeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (IBAction)backAction:(UIButton *)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
